ec309b7e62b6787440e12f1266a656b100644b8a
Mingadinga <minpearl0826@gmail.com>
2022-02-15 09:24:26 +0900

    Spring boot Init

---
+++ backend/zero-zone/src/main/java/com/dalgona/zerozone/ZeroZoneApplication.java
@@ -0,0 +1,13 @@
+package com.dalgona.zerozone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroZoneApplication.class, args);
+	}
+
+}
---
+++ backend/zero-zone/src/test/java/com/dalgona/zerozone/ZeroZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalgona.zerozone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
