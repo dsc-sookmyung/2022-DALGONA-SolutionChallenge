e43bd962569107284d7eeb0b778eb403461d1e72
whaeundo <1916388@sookmyung.ac.kr>
2022-03-27 20:11:50 +0900

    단어 북마크 등록 및 해제

--- flutter/lib/Speaking/sp_practiceview_word.dart
+++ flutter/lib/Speaking/sp_practiceview_word.dart
@@ -7,6 +7,12 @@ import 'package:video_player/video_player.dart';
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:zerozone/Login/login.dart';
+import 'package:zerozone/Login/refreshToken.dart';
+
 
 class SpWordPracticePage extends StatefulWidget {
 
@@ -37,6 +43,74 @@ class _SpWordPracticePageState extends State<SpWordPracticePage> {
 
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
+
+  void wordBookmark(int probId) async {
+
+    Map<String, String> _queryParameters = <String, String>{
+      'speakingProbId': probId.toString(),
+    };
+
+    var url = Uri.http('localhost:8080', '/bookmark/speaking', _queryParameters);
+
+    var response = await http.post(url, headers: {'Accept': 'application/json', "content-type": "application/json", "Authorization": "Bearer ${authToken}" });
+
+    print(url);
+
+    if (response.statusCode == 200) {
+      print('Response body: ${jsonDecode(utf8.decode(response.bodyBytes))}');
+
+      var body = jsonDecode(utf8.decode(response.bodyBytes));
+
+      dynamic data = body["data"];
+
+      print("북마크에 등록되었습니다.");
+    }
+    else if(response.statusCode == 401){
+      await RefreshToken(context);
+      if(check == true){
+        wordBookmark(probId);
+        check = false;
+      }
+    }
+    else {
+      print('error : ${response.reasonPhrase}');
+    }
+
+  }
+
+  void deleteWordBookmark(int probId) async {
+
+    Map<String, String> _queryParameters = <String, String>{
+      'speakingProbId': probId.toString(),
+    };
+
+    var url = Uri.http('localhost:8080', '/bookmark/speaking', _queryParameters);
+
+    var response = await http.delete(url, headers: {'Accept': 'application/json', "content-type": "application/json", "Authorization": "Bearer ${authToken}" });
+
+    print(url);
+
+    if (response.statusCode == 200) {
+      print('Response body: ${jsonDecode(utf8.decode(response.bodyBytes))}');
+
+      var body = jsonDecode(utf8.decode(response.bodyBytes));
+
+      dynamic data = body["data"];
+
+      print("북마크가 해제되었습니다.");
+    }
+    else if(response.statusCode == 401){
+      await RefreshToken(context);
+      if(check == true){
+        deleteWordBookmark(probId);
+        check = false;
+      }
+    }
+    else {
+      print('error : ${response.reasonPhrase}');
+    }
+
+  }
 
 
   @override
@@ -344,8 +418,10 @@ class _SpWordPracticePageState extends State<SpWordPracticePage> {
     setState(() {
       if (_isStared) {
         _isStared = false;
+        deleteWordBookmark(widget.probId);
       } else {
         _isStared = true;
+        wordBookmark(widget.probId);
       }
     });
   }
