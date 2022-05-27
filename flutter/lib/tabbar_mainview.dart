ec0c8477d610ca686d583182a5785c73632d0677
whaeundo <1916388@sookmyung.ac.kr>
2022-05-28 05:09:18 +0900

    Chore: Modify CustomViewMainPage Style

--- flutter/lib/tabbar_mainview.dart
+++ flutter/lib/tabbar_mainview.dart
@@ -67,7 +67,7 @@ class _BottomNavigatorState extends State<BottomNavigator> with SingleTickerProv
           Tab(
             icon: _seletedIndex == 0 ? Icon(Icons.face, color: Color(0xff5AA9DD)) : Icon(Icons.face_outlined, color: Color(0xff5AA9DD)),
             child: Text(
-              '구화',
+              '연습하기',
               style: TextStyle(color: Color(0xff5AA9DD), fontSize: 11),
             ),
           ),
