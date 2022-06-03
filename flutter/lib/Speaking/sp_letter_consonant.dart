459b78e10b1287ae7e26979a552ae810643cd147
whaeundo <1916388@sookmyung.ac.kr>
2022-06-04 00:04:05 +0900

    Feat: Add Code in Letter Speaking Practice

--- flutter/lib/Speaking/sp_letter_consonant.dart
+++ flutter/lib/Speaking/sp_letter_consonant.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:zerozone/Login/login.dart';
+import 'package:zerozone/Login/refreshToken.dart';
+import 'package:zerozone/server.dart';
 
 import 'sp_letter_vowel.dart';
 
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+class ConsonantList {
+  final String consonant;
+  final int index;
+
+  ConsonantList(this.consonant, this.index);
+}
+
 class ChooseConsonantPage extends StatefulWidget {
-  const ChooseConsonantPage({Key? key}) : super(key: key);
+
+  final List<ConsonantList> consonantList;
+  const ChooseConsonantPage({Key? key,  required this.consonantList}) : super(key: key);
 
   @override
   _ChooseConsonantPageState createState() => _ChooseConsonantPageState();
@@ -15,14 +26,73 @@ class ChooseConsonantPage extends StatefulWidget {
 
 class _ChooseConsonantPageState extends State<ChooseConsonantPage> {
 
-  List<String> consonantList = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+  // List<ConsonantList> consonantList = widget.consonantList;
+
+  final vowelList = new List<VowelList>.empty(growable: true);
+
+  Future<void> getVowel(String gridItem, int index) async {
+
+    Map<String, String> _queryParameters = <String, String>{
+      'onsetId' : index.toString(),
+      'onset' : gridItem
+    };
+
+    var url = Uri.http('${serverHttp}:8080', '/speaking/list/letter/nucleus', _queryParameters);
+
+    var response = await http.get(url, headers: {'Accept': 'application/json', "content-type": "application/json", "Authorization": "Bearer ${authToken}" });
+
+    print(url);
+
+    if (response.statusCode == 200) {
+      print('Response body: ${jsonDecode(utf8.decode(response.bodyBytes))}');
+
+      var body = jsonDecode(utf8.decode(response.bodyBytes));
+
+      dynamic data = body["data"];
+
+      print(data);
+
+      if(vowelList.length == 0){
+        for(dynamic i in data){
+          String a = i["nucleus"];
+          int b = i["id"];
+          vowelList.add(VowelList(a, b));
+        }
+      }
+
+      print("vowelList: ${vowelList}");
+
+      Navigator.push(
+          context, MaterialPageRoute(builder: (_) => ChooseVowelPage(consonant: gridItem, consonantIndex: index, vowelList: vowelList))
+      );
+      // urlInfo(letter, letterId);
+      //
+      // Navigator.of(context).pop();
+      // Navigator.push(
+      //     context, MaterialPageRoute(builder: (_) => ChooseConsonantPage(consonantList: consonantList))
+      // );
+
+    }
+    else if(response.statusCode == 401){
+      await RefreshToken(context);
+      if(check == true){
+        getVowel(gridItem, index);
+        check = false;
+      }
+    }
+    else {
+      print('error : ${response.reasonPhrase}');
+    }
+  }
 
   getGridViewSelectedItem(BuildContext context, String gridItem, int index){
     //
-    Navigator.push(
-        context, MaterialPageRoute(builder: (_) => ChooseVowelPage(consonant: gridItem, consonantIndex: index+1,))
-    );
+    getVowel(gridItem, index);
+
+
   }
+
+
 
   @override
   void initState() {
@@ -43,11 +113,7 @@ class _ChooseConsonantPageState extends State<ChooseConsonantPage> {
                   Color(0xffEFF4FA),
                   Color(0xffECF4FE),
                 ],
-                stops: [
-                  0.3,
-                  0.7,
-                  0.9,
-                ],
+                stops: [0.3, 0.7, 0.9],
               ),
             ),
             child: SafeArea(
@@ -56,10 +122,6 @@ class _ChooseConsonantPageState extends State<ChooseConsonantPage> {
                       Container(
                         margin: EdgeInsets.only(top: 20.0),
                         height: 50.0,
-                        // decoration: BoxDecoration(
-                        //   color: Colors.white,
-                        //   borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15.0), bottomRight: Radius.circular(15.0))
-                        // ),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           crossAxisAlignment: CrossAxisAlignment.center,
@@ -80,7 +142,7 @@ class _ChooseConsonantPageState extends State<ChooseConsonantPage> {
                               alignment: Alignment.center,
                               width: 300.0,
                               child: Text(
-                                "말하기 한 글자 연습",
+                                "한 글자 연습: 초성",
                                 style: TextStyle(
                                     color: Color(0xff333333),
                                     fontSize: 24,
@@ -101,38 +163,25 @@ class _ChooseConsonantPageState extends State<ChooseConsonantPage> {
                                   Expanded(
                                     child: GridView.count(
                                       crossAxisCount: 3,
-                                      children: consonantList
+                                      children: widget.consonantList
                                           .asMap()
                                           .map((index, data) => MapEntry(
                                           index,
                                           GestureDetector(
                                               onTap: () {
                                                 getGridViewSelectedItem(
-                                                    context, data, index);
+                                                    context, data.consonant, data.index);
                                               },
                                               child: Container(
-                                                  margin: EdgeInsets.symmetric(
-                                                      vertical: 15,
-                                                      horizontal: 15),
+                                                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
                                                   decoration: BoxDecoration(
-                                                      color: (index / 3) % 2 < 1
-                                                          ? Color(0xffD8EFFF)
-                                                          : Color(0xff97D5FE),
-                                                      borderRadius:
-                                                      BorderRadius.all(
-                                                          Radius.circular(
-                                                              15.0))),
+                                                      color: (index / 3) % 2 < 1 ? Color(0xffD8EFFF) : Color(0xff97D5FE),
+                                                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
                                                   child: Center(
                                                     child: Text(
-                                                      data,
-                                                      style: TextStyle(
-                                                          fontSize: 42,
-                                                          color:
-                                                          Color(0xff333333),
-                                                          fontWeight:
-                                                          FontWeight.w900),
-                                                      textAlign:
-                                                      TextAlign.center,
+                                                      data.consonant,
+                                                      style: TextStyle(fontSize: 42, color: Color(0xff333333), fontWeight: FontWeight.w900),
+                                                      textAlign: TextAlign.center,
                                                     ),
                                                   )))))
                                           .values
